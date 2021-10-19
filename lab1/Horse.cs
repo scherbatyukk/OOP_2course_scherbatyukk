e1ae71b13e9234d578fc7bc37f65921be8f62968
scherbatyukk <itsfranplay@gmail.com>
2021-10-19 10:36:42 +0300

    lab1

---
+++ lab1/Horse.cs
@@ -0,0 +1,74 @@
+using System;
+using static System.Console;
+namespace lab1
+{
+    class Horse : FarmPet
+    {
+        private DateTime birthDate;
+        private double consumptionPerDay;
+        private Horse()
+        {
+            this.birthDate = DateTime.Now;
+            this.consumptionPerDay = 0;
+        }
+        private Horse(string ownerName, string licenceID, double amountOfWaterToOvercomeThirst, double consumptionPerDay) : base(ownerName, licenceID, amountOfWaterToOvercomeThirst)
+        {
+            this.birthDate = DateTime.Now;
+            this.consumptionPerDay = consumptionPerDay;
+        }
+        public static Horse GetPet()
+        {
+            return new Horse(); 
+        }
+        public static Horse GetPet(string name, string licenceID, double amountOfWaterToOvercomeThirst, double consumptionPerDay)
+        {
+            return new Horse(name, licenceID, amountOfWaterToOvercomeThirst, consumptionPerDay); 
+        }
+        public double ConsumptionPerDay
+        {
+            get
+            {
+                return consumptionPerDay;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.consumptionPerDay = value;
+                }
+                else
+                {
+                    this.consumptionPerDay = 0;
+                }
+            }
+        }
+        public double Drinks
+        {
+            get
+            {
+                if (ConsumptionPerDay != 0)
+                {
+                    return ConsumptionPerDay / amountOfWaterToOvercomeThirst;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        public void StrokeTheHorse()
+        {
+            WriteLine("Horse is happy that you stroked her! Horse love u <3");
+        }
+        public override string GetPetDescription()
+        {
+            return $"Horse description:\n - Owner: {ownerName}\n - Licence ID: {licenceID}" + 
+            $"\n - Type: {petType}\n - Amount of water to not be thirst: {amountOfWaterToOvercomeThirst}" + 
+            $"\n - Birth Date: {birthDate.ToString("d")}";
+        }
+        ~Horse()
+        {
+            WriteLine("- Dog destructor called -");
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pet pet = new Pet("Yaroslav Scherbatyuk", "ABCD1234");
+            FarmPet farmPet = new FarmPet("Yaroslav Scherbatyuk", "ABCD1234", 5);
+            HomePet homePet = new HomePet("Yaroslav Scherbatyuk", "ABCD1234", 300);
+            Dog dog = Dog.GetPet("Ivan", "OO0000OO", 450, 900);
+
+            DisposePets();
+            Console.WriteLine("\nMemory before creating 10_000 obj: " + GC.GetTotalMemory(false));
+            for (int i = 0; i < 10_000; i++)
+            {            
+                Pet pet1 = new Pet();
+
+                pet1.Dispose();
+            }
+            Console.WriteLine($"Memory after creating 10_000 obj: {GC.GetTotalMemory(false)}\nGeneration of 'dog' obj before collecting: {GC.GetGeneration(dog)}\n");
+            GC.Collect(2);
+            GC.WaitForPendingFinalizers();
+            Console.WriteLine($"\nMemory after collecting garbage: {GC.GetTotalMemory(false)}\nGeneration of 'dog' obj after collecting: {GC.GetGeneration(dog)}\nGarbage Collection count: {GC.CollectionCount(0)}\n");
+        }
+
+        private static void DisposePets()
+        {
+            Horse horse = Horse.GetPet();
+            Dog dog = Dog.GetPet();
+            horse.Dispose();
+            dog.Dispose();
+            GC.ReRegisterForFinalize(horse);
+            GC.ReRegisterForFinalize(dog);
+        }
+    }
+}
